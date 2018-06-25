ba62bc8179b1a75ef48b0024707460244669e121
lec <lecason.g@icloud.com>
2018-06-25 23:34:48 +0800

    一个简单netty web demo 自己还不会写，代码出自碎月无恒

---
+++ demo-netty/src/main/java/com/demo/netty/TestChannelnitializer.java
@@ -0,0 +1,23 @@
+package com.demo.netty;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+/**
+ * Created by lec on 2018/6/25.
+ */
+public class TestChannelnitializer extends ChannelInitializer<SocketChannel>{
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        /**
+         * Handler就相当于Servlet中的过滤器, 请求和响应都会走Handler
+         * HttpServerCodec: http的编解码器，用于Http请求和相应
+         */
+        pipeline.addLast("httpServerCodec", new HttpServerCodec());
+        pipeline.addLast("testHttpServerHandler", new TestHttpServerHandler());
+    }
+}
